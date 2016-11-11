0d0cdb817be7a8101d32382a885e8713d549c0f8
Ipl-Abe <crowsky7275@gmail.com>
2016-11-11 13:19:00 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,10 @@
 static const int src_img_rows = 700;
 static const int src_img_cols = 800;
 
-static const double R = 1;
-static const double G = 1;
-static const double B = 0;
-
-static const int THRESH = 120; //しきい値
-
 using namespace cv;
 using namespace std;
 
-void onTrackbarChanged(int thres, void*);
+
 Point2i calculate_center(Mat);
 void getCoordinates(int event, int x, int y, int flags, void* param);
 Mat undist(Mat);
@@ -33,27 +27,25 @@ void colorExtraction(cv::Mat* src, cv::Mat* dst,
 
 Mat image1;
 Mat src_img, src_frame;
-Mat element = Mat::ones(3, 3, CV_8UC1); //追加　3×3の行列で要素はすべて1　dilate処理に必要な行列
+Mat element = Mat::ones(3, 3, CV_8UC1); //@comment 追加　3×3の行列で要素はすべて1　dilate処理に必要な行列
 int Ax, Ay, Bx, By, Cx, Cy, Dx, Dy;
 int Tr, Tg, Tb;
 Point2i pre_point; //@comment Point構造体<int型>
 
 int flag = 0;
-int ct = 0;
+//int ct = 0;
 Mat dst_img, colorExtra;
 
 ofstream ofs("out4.csv");
 
 int main(int argc, char *argv[])
 {
-	//@comment 動画の読み込み
-	//VideoCapture video("./test2.MP4");
-	//if (!video.isOpened())
-	//{
-		//return -1;
-	//}
+
+	//@comment カメラの呼び出し pcのカメラ : 0 webカメラ : 1 
 	VideoCapture cap(1);
-	if (!cap.isOpened()) return -1;
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640); //@comment webカメラの横幅を設定
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT,480); //@comment webカメラの縦幅を設定
+	if (!cap.isOpened()) return -1; //@comment 呼び出しミスがあれば終了
 
 	namedWindow("src", 1);
 	namedWindow("dst", 1);
@@ -61,9 +53,11 @@ int main(int argc, char *argv[])
 	namedWindow("test1", 1);
 	namedWindow("binari", 1);
 
-	cap >> src_frame;
-	//resize(src_frame, src_img, Size(src_img_cols, src_img_rows), CV_8UC3);
+	cap >> src_frame; //@comment 1フレーム取得
+	resize(src_frame, src_frame, Size(src_img_cols, src_img_rows), CV_8UC3); //@取得画像のリサイズ
 	//src_img = undist(src_img) ; //@comment カメラの歪みをとる(GoPro魚眼)
+
+
 	//------------------座標取得-----------------------------------------------
 	//@comment 画像中からマウスで4点を取得その後ESCキーを押すと変換処理が開始する
 
@@ -77,7 +71,7 @@ int main(int argc, char *argv[])
 
 	//------------------透視変換-----------------------------------------------
 	Point2f pts1[] = { Point2f(Ax, Ay), Point2f(Bx, By),
-		Point2f(Cx, Cy), Point2f(Dx, Dy) };
+		Point2f(Cx, Cy), Point2f(Dx, Dy) }; 
 
 	Point2f pts2[] = { Point2f(0, src_img_rows), Point2f(0, 0),
 		Point2f(src_img_cols, 0), Point2f(src_img_cols, src_img_rows) };
@@ -87,60 +81,50 @@ int main(int argc, char *argv[])
 	Mat dst_img, colorExtra;
 
 	//@comment 変換(線形補完)
-	warpPerspective(src_frame, dst_img, perspective_matrix, src_img.size(), INTER_LINEAR);
+	warpPerspective(src_frame, dst_img, perspective_matrix, src_frame.size(), INTER_LINEAR);
 
 	//@comment 変換前後の座標を描画
-	line(src_img, pts1[0], pts1[1], Scalar(255, 0, 255), 2, CV_AA);
-	line(src_img, pts1[1], pts1[2], Scalar(255, 255, 0), 2, CV_AA);
-	line(src_img, pts1[2], pts1[3], Scalar(255, 255, 0), 2, CV_AA);
-	line(src_img, pts1[3], pts1[0], Scalar(255, 255, 0), 2, CV_AA);
-	line(src_img, pts2[0], pts2[1], Scalar(255, 0, 255), 2, CV_AA);
-	line(src_img, pts2[1], pts2[2], Scalar(255, 255, 0), 2, CV_AA);
-	line(src_img, pts2[2], pts2[3], Scalar(255, 255, 0), 2, CV_AA);
-	line(src_img, pts2[3], pts2[0], Scalar(255, 255, 0), 2, CV_AA);
+	line(src_frame, pts1[0], pts1[1], Scalar(255, 0, 255), 2, CV_AA);
+	line(src_frame, pts1[1], pts1[2], Scalar(255, 255, 0), 2, CV_AA);
+	line(src_frame, pts1[2], pts1[3], Scalar(255, 255, 0), 2, CV_AA);
+	line(src_frame, pts1[3], pts1[0], Scalar(255, 255, 0), 2, CV_AA);
+	line(src_frame, pts2[0], pts2[1], Scalar(255, 0, 255), 2, CV_AA);
+	line(src_frame, pts2[1], pts2[2], Scalar(255, 255, 0), 2, CV_AA);
+	line(src_frame, pts2[2], pts2[3], Scalar(255, 255, 0), 2, CV_AA);
+	line(src_frame, pts2[3], pts2[0], Scalar(255, 255, 0), 2, CV_AA);
 
 	namedWindow("plotCoordinates", 1);
 	imshow("plotCoordinates", src_frame);
 
 	namedWindow("dst", 1);
 	imshow("dst", dst_img);
-	int frame = 0;
+
+
+	int frame = 0; //@comment フレーム数保持変数
+
 	while (1){
 
 		cap >> src_frame;
-
-		if (frame % 10 == 0){
+		
+		if (frame % 1 == 0){ //@comment　フレームの取得数を調節可能
 
 			//@comment 画像をリサイズ(大きすぎるとディスプレイに入りらないため)
-			//resize(src_frame, src_frame, Size(src_img_cols, src_img_rows), CV_8UC3);
+			resize(src_frame, src_frame, Size(src_img_cols, src_img_rows), CV_8UC3);
 			//src_frame = undist(src_frame); //@comment カメラの歪みをとる(GoPro魚眼)
 
-			//}
-			//else{
 			//--------------------グレースケール化---------------------------------------
 
-			//(2) RGB値設定 
-			int x, y;
-			uchar r1, g1, b1, d;
-			Vec3b color1;
+			//変換(線形補完)
+			warpPerspective(src_frame, dst_img, perspective_matrix, src_frame.size(), INTER_LINEAR);
 			//@comment hsvを利用して赤色を抽出
 			//入力画像、出力画像、変換、h最小値、h最大値、s最小値、s最大値、v最小値、v最大値
-			warpPerspective(src_frame, dst_img, perspective_matrix, src_frame.size(), INTER_LINEAR);
 			colorExtraction(&dst_img, &colorExtra, CV_BGR2HSV, 150, 180, 70, 255, 70, 255);
 			cvtColor(colorExtra, colorExtra, CV_BGR2GRAY);//@comment グレースケールに変換
 
 
 			//２値化
 			//------------------しきい値目測用--------------------------------------------
-
-
-			int value = 0;
-			createTrackbar("value", "binari", &value, 255, onTrackbarChanged);
-			setTrackbarPos("value", "binari", 0);
-
 			Mat binari_2;
-
-			//(2)しきい値決めうち
 
 			//----------------------二値化-----------------------------------------------
 			threshold(colorExtra, binari_2, 0, 255, THRESH_BINARY);
@@ -154,61 +138,40 @@ int main(int argc, char *argv[])
 				cout << point.x << " " << ypos << endl; //@comment 変換画像中でのロボットの座標(重心)
 				ofs << point.x << ", " << ypos << endl; //@comment 変換
 			}
-			//cout << flag<<endl;
-			//@comment 重心点のプロット 
-			//画像，円の中心座標，半径，色(青)，線太さ，種類(-1, CV_AAは塗りつぶし)
-			if (!point.y == 0){
+
+			//-------------------重心点のプロット----------------------------------------- 
+			if (!point.y == 0){ //@comment point.y == 0の場合はexceptionが起こる( 0除算 )
+				//@comment 画像，円の中心座標，半径，色(青)，線太さ，種類(-1, CV_AAは塗りつぶし)
 				circle(dst_img, Point(point.x, point.y + 6 * ((1000 / point.y) + 1)), 5, Scalar(200, 0, 0), -1, CV_AA);
 			}
 
 
 			//---------------------表示部分----------------------------------------------
-			Mat base(src_img_rows, src_img_cols * 2, CV_8UC3);
-			Mat roi1(base, Rect(0, 0, src_frame.cols, src_frame.rows));
-			src_frame.copyTo(roi1);
-			//Mat roi2(base, Rect(dst_img.cols, 0, dst_img.cols, dst_img.rows));
-			//dst_img.copyTo(roi2);
 
-			imshow("video", src_frame);
-
-			//namedWindow("dst");
-			imshow("test1", dst_img);//@comment 出力画像
-
-			//namedWindow("colorExt");
+			//imshow("video", src_frame);
+			imshow("red_point", dst_img);//@comment 出力画像
 			imshow("colorExt", colorExtra);//@comment 赤抽出画像
-
-			//cout << "frame" << ct++ << endl;
+			//cout << "frame" << ct++ << endl; //@comment frame数表示
 
 			if (src_frame.empty() || waitKey(30) >= 0)
 			{
 				destroyAllWindows();
 				return 0;
 			}
-
 		}
-		//}
-		//destroyAllWindows();
 		frame++;
 	}
-	ofs.close();
-
+	ofs.close(); //@comment ファイルストリームの解放
 }
 
-
-
-
+//@comment 2点間の距離取得関数
 double get_points_distance(Point2i point, Point2i pre_point){
 
 	return sqrt((point.x - pre_point.x) * (point.x - pre_point.x)
 		+ (point.y - pre_point.y) * (point.y - pre_point.y));
 }
 
-//@commentトラックバー操作イベントに応じた処理
-void onTrackbarChanged(int thres, void*)
-{
-
-}
-
+//@comment 重心取得用関数
 Point2i calculate_center(Mat gray)
 {
 
@@ -225,12 +188,13 @@ Point2i calculate_center(Mat gray)
 	return center;
 }
 
+//@comment 入力画像から4点を設定する関数
 void getCoordinates(int event, int x, int y, int flags, void* param)
 {
 
 	static int count = 0;
 	switch (event){
-	case CV_EVENT_LBUTTONDOWN:
+	case CV_EVENT_LBUTTONDOWN://@comment 左クリックが押された時
 
 		if (count == 0){
 			Ax = x, Ay = y;
@@ -264,25 +228,26 @@ void getCoordinates(int event, int x, int y, int flags, void* param)
 		break;
 	}
 }
-
+//@comment カメラキャリブレーション用関数(gopro用)
 Mat undist(Mat src_img)
 {
 	Mat dst_img;
 
-	//カメラマトリックス
+	//@comment カメラマトリックス(gopro)
 	Mat cameraMatrix = (Mat_<double>(3, 3) << 469.96, 0, 400, 0, 467.68, 300, 0, 0, 1);
-	//歪み行列
+	//@comment 歪み行列(gopro)
 	Mat distcoeffs = (Mat_<double>(1, 5) << -0.18957, 0.037319, 0, 0, -0.00337);
 
 	undistort(src_img, dst_img, cameraMatrix, distcoeffs);
 	return dst_img;
 }
 
+//@comment 色抽出用関数 
 void colorExtraction(cv::Mat* src, cv::Mat* dst,
 	int code,
-	int ch1Lower, int ch1Upper,
-	int ch2Lower, int ch2Upper,
-	int ch3Lower, int ch3Upper
+	int ch1Lower, int ch1Upper, //@comment H(色相)　最小、最大
+	int ch2Lower, int ch2Upper, //@comment S(彩度)　最小、最大
+	int ch3Lower, int ch3Upper  //@comment V(明度)　最小、最大
 	)
 {
 	cv::Mat colorImage;
@@ -321,21 +286,19 @@ void colorExtraction(cv::Mat* src, cv::Mat* dst,
 			}
 		}
 	}
-	//LUTを使用して二値化
+	//@comment LUTを使用して二値化
 	cv::LUT(colorImage, lut, colorImage);
 
-	//namedWindow("colorImage", 1);
-
-	//Channel毎に分解
+	//@comment Channel毎に分解
 	std::vector<cv::Mat> planes;
 	cv::split(colorImage, planes);
 
-	//マスクを作成
+	//@comment マスクを作成
 	cv::Mat maskImage;
 	cv::bitwise_and(planes[0], planes[1], maskImage);
 	cv::bitwise_and(maskImage, planes[2], maskImage);
 
-	//出力
+	//@comemnt 出力
 	cv::Mat maskedImage;
 	src->copyTo(maskedImage, maskImage);
 	*dst = maskedImage;
